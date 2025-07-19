bf95924e7e90e72f3e48caaa9999c075dfc29420
Naresh <nareshrcr20@gmail.com>
2025-07-20 02:16:03 +0530

    Test

--- MedProSmile/Controllers/EmployeeController.cs
+++ MedProSmile/Controllers/EmployeeController.cs
@@ -21,7 +21,7 @@ namespace MedProSmile.Controllers
         public async Task<IActionResult> GetAllPaged([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
         {
             var result = await _service.GetAllPagedAsync(pageNumber, pageSize);
-            return Ok(result);
+            return Ok(result); 
         }
 
         [HttpGet("getById")]
